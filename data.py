d736a051b86435c24392d66d1235b37f6e8ab75c
rajeshnaik22 <rajesh@verbit.ai>
2022-09-30 18:47:20 +0530

    Initial commit

---
+++ data.py
@@ -0,0 +1,4 @@
+data=[
+    { "question" : "Capital of India", "answer" : "Delhi"},
+    { "question": "Capital of USA", "answer": "Washington"}
+]
---
+++ food.py
@@ -0,0 +1,23 @@
+from turtle import Turtle
+
+from turtle import Turtle
+import random
+COLORS=["red","blue","green","purple","yellow"]
+
+class Food(Turtle):
+    def __init__(self):
+        super().__init__()
+        self.shape("turtle")
+        self.penup()
+        # self.shapesize(stretch_len=0.5, stretch_wid=0.5)
+        self.color(COLORS[random.randint(0,len(COLORS))-1])
+        self.speed("fastest")
+        self.refresh()
+
+        
+
+    def refresh(self):
+        random_x = random.randint(-280,280)
+        random_y = random.randint(-280,280)
+        self.goto(random_x,random_y)
+        
